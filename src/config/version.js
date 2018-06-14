3e802895ca31bd2b5b3fe632f3b8c8c8934ce54b
lxfriday <liu3248184446@outlook.com>
2018-06-14 21:37:43 +0800

    [update]:修改版本信息

--- src/config/version.js
+++ src/config/version.js
@@ -4,48 +4,56 @@
  * @author lxfriday
  */
 export default {
-  version: 'V2.0.0',
+  version: 'V2.0.2',
   releaseNote: [
+    {
+      version: 'V2.0.2',
+      note: [
+        '[update]:添加输入用户名、密码之后的记忆功能(体测、教务管理系统)',
+        '[update]:\'体育查分\' => \'体测成绩\' 命名语义化优化',
+        '[new]:添加华中农业大学校庆宣传图',
+      ],
+    },
     {
       version: 'V2.0.0',
       note: [
-        'update:取消所有UGC内容部分，只提供查询信息服务',
+        '[update]:取消所有UGC内容部分，只提供查询信息服务',
       ],
     },
     {
       version: 'V1.2.3',
       note: [
-        'update:修复安卓手机无法显示教务管理系统验证码的问题', // base64编码实际格式为：data:image/png;base64,
+        '[update]:修复安卓手机无法显示教务管理系统验证码的问题', // base64编码实际格式为：data:image/png;base64,
       ],
     },
     {
       version: 'V1.2.1',
       note: [
-        'update:修改成绩查询卡片样式',
+        '[update]:修改成绩查询卡片样式',
       ],
     },
     {
       version: 'V1.2.0',
       note: [
-        'new: 校园服务-成绩查询、等级考试成绩查询、考试信息查询；宿舍电费查询',
-        'update:消除开发者信息',
+        '[new]:校园服务-成绩查询、等级考试成绩查询、考试信息查询；宿舍电费查询',
+        '[update]:消除开发者信息',
       ],
     },
     {
       version: 'V1.1.4',
-      note: ['用户注册的昵称类型限制修复'],
+      note: ['[update]:用户注册的昵称类型限制修复'],
     },
     {
       version: 'V1.1.3',
-      note: ['在用户登录、注册、退出登录成功之后都会relaunch到about页面，保证数据的同步性'],
+      note: ['[update]:在用户登录、注册、退出登录成功之后都会relaunch到about页面，保证数据的同步性'],
     },
     {
       version: 'V1.1.2',
-      note: ['修复注册成功后页面无响应，数据不同步显示'],
+      note: ['[update]:修复注册成功后页面无响应，数据不同步显示'],
     },
     {
       version: 'V1.1.1',
-      note: ['添加了学生体育课程成绩查询'],
+      note: ['[update]:添加了学生体育课程成绩查询'],
     },
   ],
 };
